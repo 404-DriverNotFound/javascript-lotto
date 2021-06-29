8b934e68e44a75428db1d7b258a39e034c73bf65
kmin <kmin@student.42seoul.kr>
2021-06-29 18:34:24 +0900

    feat: 상수파일 작성

---
+++ src/ts/constant/constant.ts
@@ -0,0 +1,22 @@
+const AUTO: boolean = false;
+const MANUAL: boolean = true;
+const TICKET_COST = 1000;
+const MAX_TICKET_COST = 100000;
+const LOTTO_NUMBER_START = 1;
+const LOTTO_NUMBER_END = 45;
+const LOTTO_LENGTH = 6;
+
+const defaultLottery = {
+  first: -1,
+  second: -1,
+  third: -1,
+  fourth: -1,
+  fifth: -1,
+  sixth: -1,
+};
+
+export {
+  AUTO, MANUAL, TICKET_COST, MAX_TICKET_COST,
+  LOTTO_NUMBER_START, LOTTO_NUMBER_END,
+  LOTTO_LENGTH, defaultLottery,
+};
